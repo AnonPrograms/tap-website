7283f22bf370979d8daee8f6779b557f4e20c703
Lukas Akdogan <lukas.akdogan@gmail.com>
2020-11-11 16:47:38 +0100

    Update gtag.js

--- root/lib/gtag.js
+++ root/lib/gtag.js
@@ -1,4 +1,5 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js',new Date());
+gtag('set',{'user_id':'USER_ID'}); // Enter user-id with logged in user_id.
 gtag('config','UA-182111632-1');
